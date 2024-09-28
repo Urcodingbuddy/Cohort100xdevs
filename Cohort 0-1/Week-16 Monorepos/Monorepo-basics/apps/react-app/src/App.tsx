77fb9fe71d14a573c4813376de8ef6fa5abe205e
Urcodingbuddy <anshpethe110@gmail.com>
2024-09-28 12:42:58 +0530

    removed unused imports

--- Cohort 0-1/Week-16 Monorepos/Monorepo-basics/apps/react-app/src/App.tsx
+++ Cohort 0-1/Week-16 Monorepos/Monorepo-basics/apps/react-app/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import {InputBox} from '@repo/ui/input-box'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
