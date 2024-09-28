8fffa53b17b573d7718622d32864e279e3cbef6d
Urcodingbuddy <anshpethe110@gmail.com>
2024-09-29 00:30:00 +0530

    compleated monorepos

--- Cohort 0-1/Week-16 Monorepos/Monorepo-basics/apps/backend/src/index.ts
+++ Cohort 0-1/Week-16 Monorepos/Monorepo-basics/apps/backend/src/index.ts
@@ -1,7 +1,9 @@
+import {BACKEND_URL} from '@repo/common/config'
 import express from 'express';
 const app = express();
 
 app.get('/', (req, res)=>{
+    console.log(BACKEND_URL)
     res.json({
         massage:"Get req Working fine!   :)  "
     })
