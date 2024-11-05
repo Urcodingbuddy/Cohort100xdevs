04904685afcbcb9826073c4691cd18e782f4c13c
Urcodingbuddy <anshpethe110@gmail.com>
2024-11-05 21:50:17 +0530

    Client generated using spec file

--- Cohort 1-100/Week-20/Auto-Spec-gen/src/index.ts
+++ Cohort 1-100/Week-20/Auto-Spec-gen/src/index.ts
@@ -1,5 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { getUserRoute, postUserRoute } from "./routes";
+import { Hono } from 'hono'
+import { swaggerUI } from "@hono/swagger-ui";
 
 const app = new OpenAPIHono()
 
@@ -28,5 +30,7 @@ app.doc('/doc', {
     title: 'My API'
   }
 })
+
+app.get('/ui', swaggerUI({url:'/doc'}))
 
 export default app
